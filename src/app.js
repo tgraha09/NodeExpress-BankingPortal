ac7aa6b6ad74f5eebddd874a11d4eecedebec990
tgraha09 <tfire09@gmail.com>
2022-03-26 15:30:41 -0400

    Completed module

--- src/app.js
+++ src/app.js
@@ -11,14 +11,45 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', {encoding: 'UTF8'})
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync('src/json/users.json', {encoding: 'UTF8'})
+const users = JSON.parse(userData)
+
+app.get('/profile', (req, res)=>{
+    let data = {user: users[0]}
+    res.render('profile', data);
+});
+
+app.get('/credit', (req, res)=>{
+    let data = {account: accounts.credit}
+    res.render('account', data);
+});
+
+app.get('/checking', (req, res)=>{
+    let data = {account: accounts.checking}
+    res.render('account', data);
+});
+
+app.get('/savings', (req, res)=>{
+    let data = {account: accounts.savings}
+    res.render('account', data);
+});
+
 
 app.get('/', (req, res)=>{
- 
-    
-    var data = {name:'Akashdeep'}
-     
-    res.render('index', {title: 'Index'});
+    let data = {title: 'Account Summary',
+                accounts: accounts
+                }
+    //console.log(data.accounts);
+    for(let account in accounts){
+        console.log(account);
+    }
+    res.render('index', data);
 });
+
+
 
 
 app.listen(port, function() {
